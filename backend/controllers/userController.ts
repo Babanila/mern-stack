e870beb08d7e7df015be90b574c74f4287389ff5
babanila <babanila@yahoo.com>
2024-09-19 00:23:55 +0200

    feat: Add user controller registration and login  logic

--- backend/controllers/userController.ts
+++ backend/controllers/userController.ts
@@ -1,5 +1,7 @@
 import { Request, Response } from 'express';
 import asyncHandler from 'express-async-handler';
+import jwt from 'jsonwebtoken';
+import bcrypt from 'bcryptjs';
 import { User } from '../models/userModel';
 
 // @desc    Get users
@@ -10,66 +12,110 @@ const getUsers = asyncHandler(async (req: Request, res: Response) => {
     res.status(200).json(users);
 });
 
-// @desc    Get  user data
-// @route   GET    /api/users/me
-// @access  Public
-const getMe = asyncHandler(async (req: Request, res: Response) => {
-    /*
-    if (!req.body) {
-        res.status(400);
-        throw new Error('Please add a payload with email and password');
-    }
-
-    const user = await User.find({ ...req.body });
-
-    res.status(201).json(user);
-    */
-    res.status(201).json({ message: 'User data details' });
-});
-
 // @desc    Register  user
 // @route   POST    /api/users/register
 // @access  Public
 const registerUser = asyncHandler(async (req: Request, res: Response) => {
-    if (!req.body) {
+    const { name, email, password } = req.body;
+
+    if (!name || !email || !password) {
         res.status(400);
-        throw new Error('Please add a payload with name, email and password');
+        throw new Error('Please add all fields');
     }
 
-    const user = await User.create({ ...req.body });
+    // Check if user exist
+    const userExist = await User.findOne({ email });
 
-    res.status(201).json(user);
+    if (userExist) {
+        res.status(400);
+        throw new Error('User already exists.');
+    }
+
+    // Hash Password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const user = await User.create({ name, email, password: hashedPassword });
+
+    if (user) {
+        res.status(201).json({
+            id: user._id,
+            name: user.name,
+            email: user.email,
+            token: generateToken(user._id as string),
+        });
+    } else {
+        res.status(400);
+        throw new Error('Invalid user data');
+    }
 });
 
 // @desc    Login  user
 // @route   POST    /api/users/login
 // @access  Public
 const loginUser = asyncHandler(async (req: Request, res: Response) => {
-    /*
-    if (!req.body) {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
         res.status(400);
-        throw new Error('Please add a payload with email and password');
+        throw new Error('Please add all fields');
     }
 
-    const user = await User.find({ ...req.body });
+    const user = await User.findOne({ email });
 
-    res.status(201).json(user);
-    */
-    res.status(201).json({ message: 'Login Sucessful.' });
+    if (user && (await bcrypt.compare(password, user.password))) {
+        res.json({
+            id: user._id,
+            name: user.name,
+            email: user.email,
+            token: generateToken(user._id as string),
+        });
+    } else {
+        res.status(400);
+        throw new Error('Invalid credentials');
+    }
+});
+
+// @desc    Get  user data
+// @route   GET    /api/users/me
+// @access  Private
+const getMe = asyncHandler(async (req: Request, res: Response) => {
+    res.status(201).json({ message: 'User data details' });
 });
 
 // @desc    Update  user
 // @route   PUT    /api/users/:id
 // @access  Private
 const updateUser = asyncHandler(async (req: Request, res: Response) => {
-    res.status(201).json({ message: 'User updated' });
+    const updatedUser = await User.findByIdAndUpdate(req.params.id, req.body, {
+        new: true,
+    });
+
+    if (!updatedUser) {
+        res.status(400);
+        throw new Error('User not found!');
+    }
+
+    res.status(200).json(updatedUser);
 });
 
 // @desc    Delete  user
 // @route   DELETE    /api/users/:id
 // @access  Private
 const deleteUser = asyncHandler(async (req: Request, res: Response) => {
-    res.status(201).json({ message: 'User deleted' });
+    const user = await User.findByIdAndDelete(req.params.id);
+
+    if (!user) {
+        res.status(400);
+        throw new Error('User not found!');
+    }
+
+    res.status(200).json({ id: req.params.id });
 });
+
+// Generate Token
+const generateToken = (id: string) => {
+    return jwt.sign({ id }, `${process.env.JWT_SECRET}`, { expiresIn: '5h' });
+};
 
 export { getMe, getUsers, registerUser, loginUser, updateUser, deleteUser };
