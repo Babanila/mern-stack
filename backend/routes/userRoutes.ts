831274ecf5bc6317facbce52b4353a8207597769
babanila <babanila@yahoo.com>
2024-09-18 14:56:43 +0200

    feat: Add user routes

---
+++ backend/routes/userRoutes.ts
@@ -0,0 +1,16 @@
+import express from 'express';
+import {
+    getUsers,
+    createUser,
+    deleteUser,
+    updateUser,
+} from '../controllers/userController';
+
+const router = express.Router();
+
+router.get('/', getUsers);
+router.post('/', createUser);
+router.put('/:id', updateUser);
+router.delete('/:id', deleteUser);
+
+export { router as userRoute };
