e1ba2a0cd83fe57049691c2c40642c279cd6e66a
babanila <babanila@yahoo.com>
2024-09-24 11:21:02 +0200

    feat: Change register api endpoint

--- backend/routes/userRoutes.ts
+++ backend/routes/userRoutes.ts
@@ -13,7 +13,7 @@ const router = express.Router();
 
 router.get('/', getUsers);
 router.get('/me', protect, getLoggedInUser);
-router.post('/register', registerUser);
+router.post('/', registerUser);
 router.post('/login', loginUser);
 router.put('/:id', updateUser);
 router.delete('/:id', deleteUser);
