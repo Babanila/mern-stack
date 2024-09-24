115fe2efd90721ef23fb864b1e4d0f5d863a21b7
babanila <babanila@yahoo.com>
2024-09-24 07:21:18 +0200

    feat: Update dashboard component

--- frontend/src/pages/Dashboard.tsx
+++ frontend/src/pages/Dashboard.tsx
@@ -1,5 +1,32 @@
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { useAppDispatch, useAppSelector } from '../app/hooks';
+import { reset } from '../features/auth/authSlice';
+
 function Dashboard() {
-    return <div>Dashboard</div>;
+    const navigate = useNavigate();
+    const dispatch = useAppDispatch();
+
+    const { user } = useAppSelector((state) => state.auth);
+
+    useEffect(() => {
+        if (!user) {
+            navigate('/login');
+        }
+
+        return () => {
+            dispatch(reset());
+        };
+    }, [user, navigate, dispatch]);
+
+    return (
+        <>
+            <section className='heading'>
+                <h1>Welcome {user && user.name}</h1>
+                <p>Goals Dashboard</p>
+            </section>
+        </>
+    );
 }
 
 export default Dashboard;
