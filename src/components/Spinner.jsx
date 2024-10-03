8476922325c7acc177443ddd9cef733194a040a1
Babanila <Babanila@users.noreply.github.com>
2024-10-03 17:11:54 +0000

    deploy: d450dad60dda011a01e2079324cd84f43c0f5ea6

--- src/components/Spinner.jsx
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function Spinner() {
-    return (<div className='loadingSpinnerContainer'>
-            <div className='loadingSpinner'></div>
-        </div>);
-}
-exports.default = Spinner;
